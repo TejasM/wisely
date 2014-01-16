7dafdd8029ceb21edf165f29879bf742057858fe
Tejas <tejasmehta0@gmail.com>
2014-01-16 18:32:27 -0500

    Newstuff

--- wisely_project/users/urls.py
+++ wisely_project/users/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
                        url(r'^login/$', views.login, name='login'),
+                       url(r'^signup/$', views.signup, name='sign-up'),
                        url(r'^index/$', views.index, name='index'),
 )
--- wisely_project/users/views.py
+++ wisely_project/users/views.py
@@ -1,12 +1,25 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
 
 
 def login(request):
     if request.method == "POST":
         pass
-    return render(request, 'users/login.html')
+    return render(request, 'base.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        user = User.objects.create(username=request.POST["email"], email=request.POST["email"],
+                                   first_name=request.POST["first_name"],
+                                   last_name=request.POST["last_name"])
+        user.set_password(request.POST["password"])
+        user.save()
+        return redirect(reverse('user:login'))
+    return render(request, 'base.html')
 
 
 @login_required
