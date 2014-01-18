121b7318b94919822e84ed708e18d7f8b4262f40
Tejas <tejasmehta0@gmail.com>
2014-01-18 17:39:59 -0500

    celery add

--- wisely_project/wisely_project/celery.py
+++ wisely_project/wisely_project/celery.py
@@ -9,13 +9,15 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wisely_project.settings.production')
 
-app = Celery('wisely_project')
+app = Celery('wisely_project', BROKER_URL = 'amqp://tejas:next36@localhost:5672//')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
+app.conf.update(
+    CELERY_RESULT_BACKEND='djcelery.backends.cache:CacheBackend',
+)
 
 @app.task(bind=True)
 def debug_task(self):
