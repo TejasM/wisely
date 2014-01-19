d3653fcca7e88cc7ee8f49af4bb9cb832f318554
Tejas <tejasmehta0@gmail.com>
2014-01-19 18:37:56 -0500

    make runable

--- wisely_project/get_courses_file.py
+++ wisely_project/get_courses_file.py
@@ -1,3 +1,8 @@
+import sys, os
+sys.path.append('/root/wisely/wisely_project/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wisely_project.settings.production'
+from django.conf import settings
+
 from django.db.models import F
 from django.utils import timezone
 from users.tasks import get_courses
