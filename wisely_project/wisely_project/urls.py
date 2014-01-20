753fa3d908893f40501ef9bc42840fda471d8394
Tejas <tejasmehta0@gmail.com>
2014-01-19 19:49:22 -0500

    format feed

--- wisely_project/wisely_project/urls.py
+++ wisely_project/wisely_project/urls.py
@@ -17,5 +17,6 @@ urlpatterns = patterns('',
 
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^users/', include('users.urls', namespace="users")),
                        url(r'^pledges/', include('pledges.urls', namespace="pledges")),
 )
