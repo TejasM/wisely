61f8fd315dc62d4979b2c9054e25aed6e7588d0f
Tejas <tejasmehta0@gmail.com>
2014-01-25 18:49:01 -0500

    Fix detail

--- wisely_project/pledges/urls.py
+++ wisely_project/pledges/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^create/$', views.create, name='create'),
                        # /pledges/23
+                       url(r'^(?P<pledge_id>\d+)/$', views.detail, name='detail'),
                        url(r'^share/(?P<pledge_id>\d+)/$', views.share, name='share'),
                        # /pledges/23/
                        url(r'^(?P<pledge_id>\d+)/results/$', views.results, name='results'),
