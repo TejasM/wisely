efe6167ea64e86a0dfe253f68deb2149e81d48d8
Tejas <tejasmehta0@gmail.com>
2014-01-25 13:59:25 -0500

    Debug false

--- wisely_project/wisely_project/settings/production.py
+++ wisely_project/wisely_project/settings/production.py
@@ -8,8 +8,6 @@ from base import *
 # into your settings, but ImproperlyConfigured is an exception.
 from django.core.exceptions import ImproperlyConfigured
 
-DEBUG = True
-
 
 def get_env_setting(setting):
     """ Get the environment setting or return exception """
