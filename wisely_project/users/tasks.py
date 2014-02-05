b6d98a7f0c9204a80fbd4161e42e8c1c4790487b
Tejas <tejasmehta0@gmail.com>
2014-02-05 11:56:53 -0500

    Fix references to userprofile

--- wisely_project/users/tasks.py
+++ wisely_project/users/tasks.py
@@ -102,9 +102,9 @@ class CourseraScraper:
                     quiz = Quiz.objects.get(heading=quiz_coursera.select('h3')[0].find(text=True, recursive=False),
                                             course=course)
                     try:
-                        progress = Progress.objects.get(quiz=quiz, user=user.userprofile)
+                        progress = Progress.objects.get(quiz=quiz, user=user.courseraprofile)
                     except Progress.DoesNotExist:
-                        progress = Progress.objects.create(quiz=quiz, user=user.userprofile)
+                        progress = Progress.objects.create(quiz=quiz, user=user.courseraprofile)
                     progress.score = quiz_details[i].select(
                         '.course-quiz-item-score td span')[0].contents[0]
                     progress.save()
