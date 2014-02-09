d1a16578cf7681c8fdfebc76250f854fdb8887c4
Tejas <tejasmehta0@gmail.com>
2014-02-09 11:40:09 -0500

    Remove login sign up

--- wisely_project/wisely_project/urls.py
+++ wisely_project/wisely_project/urls.py
@@ -9,7 +9,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', TemplateView.as_view(template_name='base.html')),
-                       url(r'^something/$', TemplateView.as_view(template_name='base-app.html')),
                        url(r'^learn-more$', TemplateView.as_view(template_name='learn-more.html')),
                        url(r'^faq$', TemplateView.as_view(template_name='faq.html')),
                        # Examples:
