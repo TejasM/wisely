8846e5b346dd666014c48a580715342b879c84f0
Tejas <tejasmehta0@gmail.com>
2014-02-19 08:07:19 -0500

    Auth cancel attmept fix

--- wisely_project/wisely_project/settings/base.py
+++ wisely_project/wisely_project/settings/base.py
@@ -163,6 +163,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_auth.middleware.SocialAuthExceptionMiddleware',
 )
 ########## END MIDDLEWARE CONFIGURATION
 
