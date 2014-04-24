b56f0b79eb7efddb2e5c6107174ed2484d5ba3d6
Tejas <tejasmehta0@gmail.com>
2014-04-23 20:07:44 -0400

    Sign up done

--- wisely_project/wisely_project/view.py
+++ wisely_project/wisely_project/view.py
@@ -1,18 +1,21 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect, render_to_response
+from django.template import RequestContext
 
 __author__ = 'tmehta'
 
 
 def index(request):
     done = request.COOKIES.get('animation', '')
-    response = render_to_response('base-alt.html', {'done': done, 'user': request.user})
+    response = render_to_response('base-alt.html', {'done': done, 'user': request.user},
+                                  context_instance=RequestContext(request))
     response.set_cookie('animation', 'done')
     return response
 
 
 def index_alt(request):
     done = request.COOKIES.get('animation', '')
-    response = render_to_response('base-alt.html', {'done': done, 'user': request.user})
+    response = render_to_response('base-alt.html', {'done': done, 'user': request.user},
+                                  context_instance=RequestContext(request))
     response.set_cookie('animation', 'done')
     return response
