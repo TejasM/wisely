41da2a2ba761f31f13a0f5c88130308fbffd99b2
Tejas Mehta <tejasmehta0@gmail.com>
2014-06-12 20:17:05 -0400

    add script

--- wisely_project/match_id.py
+++ wisely_project/match_id.py
@@ -1,9 +1,18 @@
 __author__ = 'tmehta'
-from pandas import read_csv
+from pandas import read_csv, Series
+from users.models import Course
+import numpy as np
 
 f = read_csv('CourseSkills-edXCourses.csv')
 
 courses = f['Course Name']
+f['Course Id'] = Series(np.random.randn(f.shape[0]), index=f.index)
 
-for c in courses:
-    c = c.strip()
+for i, c in enumerate(courses):
+    if isinstance(c, str):
+        c = c.strip()
+        django_course = Course.objects.filter(title__contains=c)
+        if django_course.count() != 0:
+            f['Course Id'][i] = django_course[0].course_id
+
+f.to_csv('CourseSkills-edXCourses.csv')
